be482d03f743a584d66a5f45f3e659a8d1264fc7
Vinícius Vieira De Andrade Correia <53685550837@info.local>
2024-04-25 16:39:21 -0300

    att api com metodo usuario atualizar foto

--- API-VitalHub/WebAPI/WebAPI/Controllers/UsuarioController.cs
+++ API-VitalHub/WebAPI/WebAPI/Controllers/UsuarioController.cs
@@ -5,6 +5,7 @@ using System.IdentityModel.Tokens.Jwt;
 using WebAPI.Domains;
 using WebAPI.Interfaces;
 using WebAPI.Repositories;
+using WebAPI.Utils.BlobStorage;
 using WebAPI.ViewModels;
 
 namespace WebAPI.Controllers
@@ -45,6 +46,39 @@ namespace WebAPI.Controllers
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPut("AlterarFotoPerfil")]
+        public async Task<IActionResult> UploadProfileImage(Guid id, [FromForm] UsuarioViewModel user)
+        {
+            try
+            {
+                Usuario usuarioBuscado = usuarioRepository.BuscarPorId(id);
+
+                if (usuarioBuscado == null)
+                {
+                    return NotFound();
+                }
+
+                // Logica para o upload de imagem
+                var connectionString = "DefaultEndpointsProtocol=https;AccountName=vitalmikaelg10;AccountKey=LZcV9x7wPD6ZFOJfJFXkO3EZ3Gp+UnMFwzk5X2hJgoo3kg4JhCUZ53jSMjd2M0ZHV+z4/PLuTZAd+ASt19O3Iw==;EndpointSuffix=core.windows.net";
+
+                var containerName = "containervitalg10";
+
+                string fotoUrl = await AzureBlobStorageHelper.UploadImageBlobAsync(user.Arquivo!, connectionString, containerName);
+                // Fim da logica de upload de imagem
+
+                //usuarioBuscado.Foto = fotoUrl;
+
+                usuarioRepository.AtualizarFoto(id, fotoUrl);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
     }
